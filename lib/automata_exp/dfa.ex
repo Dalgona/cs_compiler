481a7915bfc94120355ae993853dfae7d2cd51a1
Dalgona <dalgona@hontou.moe>
2018-04-19 19:41:07 +0900

    Add an accumulator and a hook to DFA

--- lib/automata_exp/dfa.ex
+++ lib/automata_exp/dfa.ex
@@ -10,6 +10,8 @@ defmodule AutomataExp.DFA do
   @type t :: %__MODULE__{}
   @type state :: term()
   @type transition_fn :: (state(), integer() -> state() | nil)
+  @type hook_fn :: (state(), term(), integer() -> term())
+  @type result :: {:accepted | :rejected, term(), charlist()}
 
   @spec new([state()], charlist(), transition_fn(), state(), [state()]) :: t()
   def new(q, s, d, q0, f) do
@@ -34,25 +36,31 @@ defmodule AutomataExp.DFA do
     }
   end
 
-  @spec run(t(), charlist()) :: any() # TODO (the same as do_run/3)
-  def run(dfa, chars) do
-    do_run(dfa, dfa.initial_state, chars)
+  @spec run(t(), charlist(), term(), hook_fn()) :: result()
+  def run(dfa, chars, acc \\ nil, hook_fn \\ fn _, a, _ -> a end)
+
+  def run(dfa, chars, acc, hook_fn) do
+    do_run(dfa, dfa.initial_state, chars, acc, hook_fn)
   end
 
-  @spec do_run(t(), state(), charlist()) :: any() # TODO
-  def do_run(dfa, state, chars)
+  @spec do_run(t(), state(), charlist(), term(), hook_fn()) :: result()
+  def do_run(dfa, state, chars, acc, hook_fn)
 
-  def do_run(dfa, state, []) do
-    MapSet.member?(dfa.accepted_states, state) && :accepted || :rejected
+  def do_run(dfa, state, [], acc, _hook_fn) do
+    if MapSet.member?(dfa.accepted_states, state) do
+      {:accepted, acc, []}
+    else
+      {:rejected, acc, []}
+    end
   end
 
-  def do_run(dfa, state, [char | chars]) do
+  def do_run(dfa, state, [char | chars], acc, hook_fn) do
     case dfa.transition_fn.(state, char) do
       nil ->
-        :rejected
+        {:rejected, acc, [char | chars]}
 
-      next_state ->
-        do_run(dfa, next_state, chars)
+      new_state ->
+        do_run(dfa, new_state, chars, hook_fn.(new_state, acc, char), hook_fn)
     end
   end
 end
