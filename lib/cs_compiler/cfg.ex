f8390a6b4c6037c2263de0f5f590e13e05587450
Dalgona <dalgona@hontou.moe>
2018-05-31 08:51:31 +0900

    Implement FIRST function

--- lib/cs_compiler/cfg.ex
+++ lib/cs_compiler/cfg.ex
@@ -11,6 +11,8 @@ defmodule CSCompiler.CFG do
   @type symbol :: nonterminal() | terminal() | nil
   @type prod :: {nonterminal(), symbol()}
 
+  @typep first_table :: %{optional(nonterminal()) => MapSet.t(terminal())}
+
   @spec new([nonterminal()], [terminal()], [prod()], nonterminal()) :: t()
   def new(vn, vt, p, s) do
     unless Enum.all?(vn, &is_atom/1) do
@@ -65,6 +67,54 @@ defmodule CSCompiler.CFG do
 
     update_nullables(MapSet.union(vn_e, tmp), vn_e, p)
   end
+
+  @spec build_first(t()) :: first_table()
+  def build_first({_vn, _vt, p, _s}) do
+    table = for {lhs, _} <- p, into: %{}, do: {lhs, MapSet.new()}
+
+    table =
+      Enum.reduce(p, table, fn
+        {lhs, [nil]}, acc ->
+          %{acc | lhs => MapSet.put(acc[lhs], nil)}
+
+        {lhs, [terminal | _rest]}, acc when not is_atom(terminal) ->
+          %{acc | lhs => MapSet.put(acc[lhs], terminal)}
+
+        _, acc ->
+          acc
+      end)
+
+    do_build_first(table, %{}, p)
+  end
+
+  @spec do_build_first(first_table(), first_table(), [prod()]) :: first_table()
+  defp do_build_first(table, table_old, p)
+  defp do_build_first(table, table, _p), do: table
+
+  defp do_build_first(table, _table_old, p) do
+    new_table =
+      Enum.reduce(p, table, fn
+        {_lhs, [nil]}, acc ->
+          acc
+
+        {lhs, rhs}, acc ->
+          %{acc | lhs => MapSet.union(acc[lhs], get_first(acc, rhs))}
+      end)
+
+    do_build_first(new_table, table, p)
+  end
+
+  @spec get_first(first_table(), list() | symbol()) :: MapSet.t(terminal())
+  defp get_first(table, symbol_or_sentential_form)
+
+  defp get_first(table, [sym | syms]) do
+    [sym | syms]
+    |> Enum.map(&get_first(table, &1))
+    |> ring_sum()
+  end
+
+  defp get_first(_table, a) when not is_atom(a), do: MapSet.new([a])
+  defp get_first(table, x), do: table[x] || MapSet.new()
 
   @spec ring_sum([MapSet.t()]) :: MapSet.t()
   def ring_sum([set | sets]) do
