6fa048231941e67c32c6a0b7cdcc013cc3124f92
Dalgona <dalgona@hontou.moe>
2018-06-01 23:49:39 +0900

    Implement LL(1) predictive parser

--- lib/cs_compiler/cfg/ll1/predictive_parser.ex
+++ lib/cs_compiler/cfg/ll1/predictive_parser.ex
@@ -15,6 +15,9 @@ defmodule CSCompiler.CFG.LL1.PredictiveParser do
           }
         }
 
+  @type input_sym :: CFG.terminal() | :end
+  @type stack_sym :: CFG.symbol() | :end
+
   @spec new(CFG.t()) :: t()
   def new(cfg) do
     vn_e = CFG.nullables(cfg)
@@ -55,5 +58,38 @@ defmodule CSCompiler.CFG.LL1.PredictiveParser do
     |> Enum.reduce(table, fn {nt, t, prod}, acc ->
       put_in(acc, [nt, t], prod)
     end)
+  end
+
+  @spec run([CFG.terminal()], t()) :: any() # TODO
+  def run(chars, parser)
+
+  def run(chars, %__MODULE__{cfg: {_, _, _, s}, table: table}) do
+    input = Enum.reverse([:end | Enum.reverse(chars)])
+    stack = [s, :end]
+
+    do_run(input, stack, table)
+  end
+
+  @spec do_run([input_sym()], [stack_sym()], table()) :: any() # TODO
+  defp do_run(input, stack, table)
+
+  defp do_run([:end], [:end], _table) do
+    :accepted
+  end
+
+  defp do_run([x | input], [x | stack], table) do
+    do_run(input, stack, table)
+  end
+
+  defp do_run([x | input], [tos | stack], table) do
+    case get_in(table, [tos, x]) do
+      {_lhs, [nil]} -> do_run([x | input], stack, table)
+      {_lhs, rhs} -> do_run([x | input], List.flatten([rhs | stack]), table)
+      nil -> :error
+    end
+  end
+
+  defp do_run(_, _, _table) do
+    :error
   end
 end
