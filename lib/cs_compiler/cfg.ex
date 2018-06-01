25ae0b5781556f316da05c7235c126c1dc9c3ad9
Dalgona <dalgona@hontou.moe>
2018-06-01 21:58:37 +0900

    Implement LL(1) table builder

--- lib/cs_compiler/cfg.ex
+++ lib/cs_compiler/cfg.ex
@@ -201,6 +201,35 @@ defmodule CSCompiler.CFG do
     |> Enum.uniq()
   end
 
+  @spec build_ll_table(t(), first_table(), follow_table()) :: any() # TODO
+  def build_ll_table({vn, _vt, p, _s}, first, follow) do
+    table = for v <- vn, into: %{}, do: {v, %{}}
+
+    p
+    |> Enum.map(fn {lhs, rhs} = prod ->
+      first_of_rhs = get_first(rhs, first)
+      follow_of_lhs = follow[lhs]
+
+      temp1 =
+        first_of_rhs
+        |> Enum.reject(&is_nil/1)
+        |> Enum.map(&{lhs, &1, prod})
+
+      temp2 =
+        if nil in first_of_rhs do
+          Enum.map(follow_of_lhs, &{lhs, &1, prod})
+        else
+          []
+        end
+
+      temp1 ++ temp2
+    end)
+    |> List.flatten()
+    |> Enum.reduce(table, fn {nt, t, prod}, acc ->
+      put_in(acc, [nt, t], prod)
+    end)
+  end
+
   @spec ll?(t()) :: boolean()
   def ll?(cfg) do
     vn_e = nullables(cfg)
