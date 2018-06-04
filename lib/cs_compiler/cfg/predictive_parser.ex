33d92c3f5500eb8e261209d1785d7649dbc5f4fa
Dalgona <dalgona@hontou.moe>
2018-06-04 22:52:35 +0900

    Introduce token-to-terminal matcher to the parser

--- lib/cs_compiler/cfg/predictive_parser.ex
+++ lib/cs_compiler/cfg/predictive_parser.ex
@@ -2,11 +2,12 @@ defmodule CSCompiler.CFG.PredictiveParser do
   alias CSCompiler.CFG
   alias CSCompiler.CFG.LL1
 
-  defstruct [:cfg, :table]
+  defstruct [:cfg, :table, :matcher]
 
   @type t :: %__MODULE__{
           cfg: CFG.t(),
-          table: LL1.ll1_table()
+          table: LL1.ll1_table(),
+          matcher: matcher_fn()
         }
 
   @type table :: %{
@@ -15,6 +16,7 @@ defmodule CSCompiler.CFG.PredictiveParser do
           }
         }
 
+  @type matcher_fn :: (term() -> term())
   @type input :: [input_sym()]
   @type stack :: [stack_sym()]
   @type input_sym :: CFG.terminal() | :end
@@ -23,8 +25,8 @@ defmodule CSCompiler.CFG.PredictiveParser do
   @type parse_error :: {:error, input()}
   @typep make_tree_result :: {parse_tree(), input(), stack()}
 
-  @spec new(CFG.t()) :: t()
-  def new(cfg) do
+  @spec new(CFG.t(), matcher_fn()) :: t()
+  def new(cfg, matcher \\ &(&1)) do
     vn_e = CFG.nullables(cfg)
     first = LL1.build_first(cfg)
     follow = LL1.build_follow(cfg, first, vn_e)
@@ -32,7 +34,8 @@ defmodule CSCompiler.CFG.PredictiveParser do
 
     %__MODULE__{
       cfg: cfg,
-      table: table
+      table: table,
+      matcher: matcher
     }
   end
 
@@ -68,11 +71,11 @@ defmodule CSCompiler.CFG.PredictiveParser do
   @spec run([CFG.terminal()], t()) :: {:ok, parse_tree()} | parse_error()
   def run(chars, parser)
 
-  def run(chars, %__MODULE__{cfg: {_, _, _, s}, table: table}) do
+  def run(chars, %__MODULE__{cfg: {_, _, _, s}, table: table, matcher: matcher}) do
     input = Enum.reverse([:end | Enum.reverse(chars)])
     stack = [s, :end]
 
-    case make_tree(input, stack, table) do
+    case make_tree(input, stack, table, matcher) do
       {{:node, _, _} = node, [:end], [:end]} ->
         {:ok, node}
 
@@ -84,15 +87,19 @@ defmodule CSCompiler.CFG.PredictiveParser do
     end
   end
 
-  @spec make_tree(input(), stack(), table()) :: make_tree_result() | parse_error()
-  defp make_tree(input, stack, table)
+  @spec make_tree(input(), stack(), table(), matcher_fn()) :: make_tree_result() | parse_error()
+  defp make_tree(input, stack, table, matcher)
 
-  defp make_tree([x | input], [x | stack], _table) do
-    {{:node, x, []}, input, stack}
+  defp make_tree([a | input], [x | stack], _table, matcher) when not is_atom(x) do
+    if matcher.(a) == x do
+      {{:node, a, []}, input, stack}
+    else
+      {:error, [a | input]}
+    end
   end
 
-  defp make_tree([a | _as] = input, [tos | stack], table) do
-    case get_in(table, [tos, a]) do
+  defp make_tree([a | _as] = input, [tos | stack], table, matcher) do
+    case get_in(table, [tos, matcher.(a)]) do
       nil ->
         {:error, input}
 
@@ -102,7 +109,7 @@ defmodule CSCompiler.CFG.PredictiveParser do
       {lhs, rhs} ->
         expanded = List.flatten([rhs | stack])
 
-        case make_tree_rec(rhs, input, expanded, table, []) do
+        case make_tree_rec(rhs, input, expanded, table, matcher, []) do
           {children, rest_input, rest_stack} ->
             {{:node, lhs, children}, rest_input, rest_stack}
 
@@ -112,22 +119,22 @@ defmodule CSCompiler.CFG.PredictiveParser do
     end
   end
 
-  defp make_tree(input, _stack, _table) do
+  defp make_tree(input, _stack, _table, _matcher) do
     {:error, input}
   end
 
-  @spec make_tree_rec([CFG.symbol()], input(), stack(), table(), [parse_tree()]) ::
+  @spec make_tree_rec([CFG.symbol()], input(), stack(), table(), matcher_fn(), [parse_tree()]) ::
           {[parse_tree()], input(), stack()} | parse_error()
-  defp make_tree_rec(rhs, input, stack, table, acc)
+  defp make_tree_rec(rhs, input, stack, table, matcher, acc)
 
-  defp make_tree_rec([], input, stack, _table, acc) do
+  defp make_tree_rec([], input, stack, _table, _matcher, acc) do
     {Enum.reverse(acc), input, stack}
   end
 
-  defp make_tree_rec([_sym | syms], input, stack, table, acc) do
-    case make_tree(input, stack, table) do
+  defp make_tree_rec([_sym | syms], input, stack, table, matcher, acc) do
+    case make_tree(input, stack, table, matcher) do
       {tree, rest_input, rest_stack} ->
-        make_tree_rec(syms, rest_input, rest_stack, table, [tree | acc])
+        make_tree_rec(syms, rest_input, rest_stack, table, matcher, [tree | acc])
 
       {:error, _} = error ->
         error
